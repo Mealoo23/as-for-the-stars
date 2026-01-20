b67f0ac4896aac8a10f7ff5d6714c2354d2c611b
Robert Panek <robbpanek@gmail.com>
2026-01-20 12:06:49 -0800

    Comment out BirthChartForm component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import BirthChartForm from "../components/BirthChartForm";
+///import BirthChartForm from "../components/BirthChartForm";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function Home() {
           zIndex: 0,
         }}
       />
-      <BirthChartForm />
+      {/* <BirthChartForm /> */}
     </div>
   );
 }
