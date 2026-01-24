010b25ed905e499a74e203eef865e792ae026294
Robert Panek <robbpanek@gmail.com>
2026-01-24 14:07:34 -0800

    Remove custom background image and revert to default Next.js layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,21 +14,10 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div className={`${geistSans.className} ${geistMono.className} relative min-h-screen`}>
-      <img
-        src="/background.png"
-        alt="Background"
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-          zIndex: 0,
-        }}
-      />
-      {/* <BirthChartForm /> */}
+    <div className={`${geistSans.className} ${geistMono.className}`}>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {/* <BirthChartForm /> */}
+      </main>
     </div>
   );
 }
