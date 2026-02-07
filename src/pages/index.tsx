dd42c598f641b3520d7ecbc4d14fca38f0fe915d
Meoraine <robbpanek@gmail.com>
2026-02-07 08:40:29 -0800

    hoveragain

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,43 @@
 import React, { useState } from 'react';
 
-const Homepage: React.FC = () => {
-  const [buttonHovered, setButtonHovered] = useState<boolean>(false);
-
-  const handleButtonHover = (hovered: boolean) => {
-    setButtonHovered(hovered);
-  };
-
-  const buttons = Array.from({ length: 5 }, (_, index) => (
-    <div key={index} style={{ position: 'fixed', right: '4px', top: `${index * 100}px` }}>
-      <StarButton buttonHovered={buttonHovered} setButtonHovered={handleButtonHover} />
-    </div>
-  ));
-
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 relative z-10">
-      <img
-        src="/background1.png"
-        alt="Background"
+const StarButton: React.FC<{ buttonHovered: boolean, setButtonHovered: (hovered: boolean) => void }> = ({ buttonHovered, setButtonHovered }) => (
+  <button
+    style={{
+      position: 'relative',
+      width: '100px',
+      height: '100px',
+      cursor: 'pointer',
+    }}
+    onMouseEnter={() => {
+      setButtonHovered(true);
+    }}
+    onMouseLeave={() => {
+      setButtonHovered(false);
+    }}
+  >
+    <img src="/star-button.png" alt="Star Button" style={{ width: '100%', height: '100%' }} />
+    {buttonHovered && (
+      <div
         style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
+          position: 'absolute',
+          top: '0',
+          left: '0',
           width: '100%',
           height: '100%',
-          objectFit: 'cover',
-          zIndex: 0,
+          backgroundColor: 'rgba(255, 255, 0, 0.5)',
+          opacity: 0,
+          transition: 'opacity 0.2s ease-in-out',
+        }}
+        onMouseEnter={() => {
+          setButtonHovered(true);
+        }}
+        onMouseLeave={() => {
+          setButtonHovered(false);
         }}
       />
-      <main className="flex flex-col items-center justify-between">
-        {buttons}
-      </main>
-    </div>
-  );
-};
+    )}
+  </button>
+);
 
 const Homepage: React.FC = () => {
   const [buttonHovered, setButtonHovered] = useState<boolean>(false);
